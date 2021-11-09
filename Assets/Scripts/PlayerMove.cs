03b4a4730dd9a9cb8f015dd33f518534c6d38f22
Crona Xu <cx640@nyu.edu>
2021-11-09 10:07:21 -0500

    PlayerMove Script Created

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+    public float gravityMultiplier;
+    bool onFloor;
+
+    Rigidbody2D myBody;
+
+    SpriteRenderer myRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
