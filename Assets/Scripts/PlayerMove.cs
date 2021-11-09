0c98815712d528eafaa92935ede40240978806d6
Crona Xu <cx640@nyu.edu>
2021-11-09 10:17:35 -0500

    PlayerMove Updated

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -24,6 +24,60 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (onFloor && myBody.velocity.y > 0.1)
+        {
+            onFloor = false;
+        }
+        CheckKeys();
+        JumpPhysics();
+    }
+
+    void CheckKeys()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRenderer.flipX = false;
+            HandleLRMovement(speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            myRenderer.flipX = true;
+            HandleLRMovement(-speed);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.W) && onFloor)
+        {
+            myBody.velocity = new Vector3(myBody.velocity.x, jumpHeight);
+
+        }
         
+    }
+
+    void JumpPhysics()
+    {
+        if (myBody.velocity.y < 0)
+        {
+            myBody.velocity += Vector2.up * Physics2D.gravity.y * (gravityMultiplier - 1f) * Time.deltaTime;
+        }
+    }
+
+    void HandleLRMovement(float dir)
+    {
+        myBody.velocity = new Vector3(dir, myBody.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "floor")
+        {
+            onFloor = true;
+            
+
+            myBody.velocity = new Vector3(myBody.velocity.x, 0);
+        }
+
     }
 }
