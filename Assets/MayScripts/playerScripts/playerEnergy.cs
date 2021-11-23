f7d9b856e73543dadbe5af461c83accbd928d39d
Lilinamay <rc4333@nyu.edu>
2021-11-23 09:22:22 -0500

    energyWork

--- Assets/MayScripts/playerScripts/playerEnergy.cs
+++ Assets/MayScripts/playerScripts/playerEnergy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class playerEnergy : MonoBehaviour
 {
+    public float energy = 1;
+    public float enToLife = 0.25f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class playerEnergy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if()
     }
 }
